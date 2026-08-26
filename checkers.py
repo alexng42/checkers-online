71fd228de3522fb5000c003ccef1b28e27ed556b
XReykoX <108241786+XReykoX@users.noreply.github.com>
2026-08-26 14:55:31 -0700

    forced jumping works

--- checkers.py
+++ checkers.py
@@ -1,6 +1,8 @@
 """
 todo:
 - forced jumps
+    check "nearby" pieces after most recent move for forced jumps
+
 - king
 - game over
 
@@ -33,67 +35,110 @@ class Checkers:
 
     def get_available_moves(self, piece_choice, select_row, select_col):
             valid_moves = []
+
+            # fix bounds
+
             # check for forced jumps
             if piece_choice.startswith("B"):
                 # left forced jump
-                if self.board[select_row - 1][select_col - 1].startswith("R") and self.board[select_row - 2][select_col - 2] == "_":
+                if (select_col - 2 >= 0 and select_row - 2 >= 0 and self.board[select_row - 1][select_col - 1].startswith("R") and self.board[select_row - 2][select_col - 2] == "_"):
                     valid_moves.append("JL")
                 # right forced jump
-                if self.board[select_row - 1][select_col + 1].startswith("R") and self.board[select_row - 2][select_col + 2] == "_":
+                if (select_col + 2 <= 7 and select_row - 2 >= 0 and self.board[select_row - 1][select_col + 1].startswith("R") and self.board[select_row - 2][select_col + 2] == "_"):
                     valid_moves.append("JR")
                 if not valid_moves:
                     # left
-                    if self.board[select_row - 1][select_col - 1] == "_":
+                    if (select_col - 1 >= 0 and select_row - 1 >= 0 and self.board[select_row - 1][select_col - 1] == "_"):
                         valid_moves.append("L")
                     # right
-                    if self.board[select_row - 1][select_col + 1] == "_":
+                    if (select_col + 1 <= 7 and select_row - 1 >= 0 and self.board[select_row - 1][select_col + 1] == "_"):
                         valid_moves.append("R")
                 return valid_moves
             else:
                 # left forced jump
-                if self.board[select_row + 1][select_col - 1].startswith("B") and self.board[select_row + 2][select_col - 2] == "_":
+                if (select_col - 2 >= 0 and select_row + 2 <= 7 and self.board[select_row + 1][select_col - 1].startswith("B") and self.board[select_row + 2][select_col - 2] == "_"):
                     valid_moves.append("JL")
                 # right forced jump
-                if self.board[select_row + 1][select_col + 1].startswith("B") and self.board[select_row + 2][select_col + 2] == "_":
+                if (select_col + 2 <= 7 and select_row + 2 <= 7 and self.board[select_row + 1][select_col + 1].startswith("B") and self.board[select_row + 2][select_col + 2] == "_"):
                     valid_moves.append("JR")
                 if not valid_moves:
                     # left
-                    if self.board[select_row + 1][select_col - 1] == "_":
+                    if (select_col - 1 >= 0 and select_row + 1 <= 7 and self.board[select_row + 1][select_col - 1] == "_"):
                         valid_moves.append("L")
                     # right
-                    if self.board[select_row + 1][select_col + 1] == "_":
+                    if (select_col + 1 <= 7 and select_row + 1 <= 7 and self.board[select_row + 1][select_col + 1] == "_"):
                         valid_moves.append("R")
                 return valid_moves
+
+    def has_forced_jumps(self, current_turn):
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                piece = self.board[r][c]
+                if piece.startswith(current_turn):
+                    if "JL" in self.get_available_moves(piece, r, c) or "JR" in self.get_available_moves(piece, r, c):
+                        return True
+        return False
 
     def process_input(self):
         print(f"{self.current_turn}'s Turn")
 
         while True:
-            piece_choice = (
-                input(f"{self.current_turn}: Type the piece you want to move: ")
-                .strip()
-                .upper()
-            )
+            if not self.has_forced_jumps(self.current_turn):
+                piece_choice = (
+                    input(f"{self.current_turn}: Type the piece you want to move: ")
+                    .strip()
+                    .upper()
+                )
 
-            if not piece_choice.startswith(self.current_turn):
-                print(f"INVALID MOVE. Select a {self.current_turn} piece")
-                continue
+                if not piece_choice.startswith(self.current_turn):
+                    print(f"INVALID MOVE. Select a {self.current_turn} piece")
+                    continue
 
-            coords = self.find_piece(piece_choice)
-            if coords is None:
-                print(f"{piece_choice} NOT FOUND. Select a different piece: ")
-                continue
+                coords = self.find_piece(piece_choice)
+                if coords is None:
+                    print(f"{piece_choice} NOT FOUND. Select a different piece: ")
+                    continue
 
-            select_row, select_col = coords
+                select_row, select_col = coords
 
-            print(f"Selected {piece_choice} at row {select_row}, col {select_col}")
+                print(f"Selected {piece_choice} at row {select_row}, col {select_col}")
 
-            piece_moves = self.get_available_moves(piece_choice, select_row, select_col)
+                piece_moves = self.get_available_moves(piece_choice, select_row, select_col)
 
-            direction = (
-                input(f"Moving {piece_choice}. Choose an option: {piece_moves} ").strip().upper()
-            )
-            return piece_choice, select_row, select_col, direction, piece_moves
+                direction = (
+                    input(f"Moving {piece_choice}. Choose an option: {piece_moves} ").strip().upper()
+                )
+                return piece_choice, select_row, select_col, direction, piece_moves
+            else:
+                piece_choice = (
+                    input(f"{self.current_turn}: Type the piece you want to move: ")
+                    .strip()
+                    .upper()
+                )
+
+                if not piece_choice.startswith(self.current_turn):
+                    print(f"INVALID MOVE. Select a {self.current_turn} piece")
+                    continue
+
+                coords = self.find_piece(piece_choice)
+                if coords is None:
+                    print(f"{piece_choice} NOT FOUND. Select a different piece: ")
+                    continue
+
+                select_row, select_col = coords
+
+                piece_moves = self.get_available_moves(piece_choice, select_row, select_col)
+
+                if "JL" not in piece_moves and "JR" not in piece_moves:
+                    print("Select the piece with forced jump. Try again.")
+                    continue
+
+                print(f"Selected {piece_choice} at row {select_row}, col {select_col}")
+
+                direction = (
+                    input(f"Moving {piece_choice}. Choose an option: {piece_moves} ").strip().upper()
+                )
+                return piece_choice, select_row, select_col, direction, piece_moves
         
 
     def make_move(self, piece_choice, select_row, select_col, direction, piece_moves):
